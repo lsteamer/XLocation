41777906eb2143e8f4dce4eb4fdebbf49955cdfd
lsteamer <lsteamer@gmail.com>
2017-05-30 11:35:52 +0200

    01 - Login Screen -No Functionality- and Basic List

---
+++ app/src/main/java/com/elmexicano/lsteamer/xlocation/LocationListFragment.java
@@ -0,0 +1,32 @@
+package com.elmexicano.lsteamer.xlocation;
+
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class LocationListFragment extends Fragment {
+
+
+    public LocationListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+}
